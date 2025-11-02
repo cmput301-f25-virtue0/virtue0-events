81333173b3e0f9770ef26ecf2a9afa18ebe520bc
rCaptainKT <144297314+rCaptainKT@users.noreply.github.com>
2025-11-02 01:44:16 -0700

    implemented getEvent, added javadoc comments

--- LotteryEventApp/app/src/main/java/com/example/lotteryeventapp/DataModel.java
+++ LotteryEventApp/app/src/main/java/com/example/lotteryeventapp/DataModel.java
@@ -6,6 +6,9 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+/**
+ * Used for interactions with the database.
+ */
 public class DataModel {
     private final FirebaseFirestore db;
     private final CollectionReference events;
@@ -27,12 +30,46 @@ public class DataModel {
 
     }
 
-    public interface Callback {
+    /**
+     * A callback for getting data from the database
+     */
+    public interface GetCallback {
+        /**
+         * Called on successful fetch from database
+         * @param obj instance of the data retrieved from the database
+         */
         void onSuccess(Object obj);
+
+        /**
+         * Called on failed fetch from database
+         * @param e error message
+         */
         void onError(Exception e);
     }
 
-    public void getEvent(String eventId, Callback cb) {
+    /**
+     * A callback for sending data from the database
+     */
+    public interface SetCallback {
+        /**
+         * Called on successful data transfer to database
+         * @param id unique id of newly added document
+         */
+        void onSuccess(String id);
+
+        /**
+         * Called on failed data transfer to database
+         * @param e error message
+         */
+        void onError(Exception e);
+    }
+
+    /**
+     * Attempts to retrieve an event from the database. The retrieved event or an error is passed to the callback.
+     * @param eventId event's unique id
+     * @param cb instance of GetCallback
+     */
+    public void getEvent(String eventId, GetCallback cb) {
         DocumentReference eventRef = this.events.document(eventId);
         eventRef.get()
                 .addOnSuccessListener(eventSnap -> {
@@ -42,6 +79,7 @@ public class DataModel {
                         String registrationDeadline = eventSnap.getString("registration_deadline");
                         String details = eventSnap.getString("details");
                         Boolean geolocation = eventSnap.getBoolean("track_geolocation");
+                        Boolean redraw = eventSnap.getBoolean("redraw");
                         int waitlistLimit;
                         if (eventSnap.getLong("waitlist_limit") != null) {
                             waitlistLimit = eventSnap.getLong("waitlist_limit").intValue();
@@ -65,6 +103,21 @@ public class DataModel {
                 }).addOnFailureListener(e -> {
                     Log.w("Firestore", "Firestore fetch failed: " + e.getMessage());
                     cb.onError(new RuntimeException("Could not fetch from server"));
+                });
+    }
+
+    /**
+     * Adds the given instance of Event to database. The event's unique id or an error is passed to the callback.
+     * @param event Event instance to be added to database
+     * @param cb instance of SetCallback
+     */
+    public void addEvent(Event event, SetCallback cb) {
+        this.events.add(event)
+                .addOnSuccessListener(documentReference -> {
+                    cb.onSuccess(documentReference.getId());
+                })
+                .addOnFailureListener(e -> {
+                    cb.onError(new RuntimeException("Could not create event"));
                 });
     }
 }
---
+++ LotteryEventApp/app/src/test/java/com/example/lotteryeventapp/DataModelTest.java
@@ -0,0 +1,60 @@
+package com.example.lotteryeventapp;
+
+import static org.junit.Assert.assertEquals;
+
+import android.util.Log;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DataModelTest {
+    public Event mockEvent() {
+        return new Event("6:00pm, January 27, 2025", "Southgate","11:59pm, December 1,2024",
+                "Stuff will happen",false,false,20,20);
+    }
+
+    /*
+    These have to be instrumented tests. I can't run those so I'll finished these later.
+     */
+
+
+//    @Test
+//    public void addEventTest() {
+//        DataModel model = new DataModel();
+//        Event event = mockEvent();
+//
+//        model.addEvent(event, new DataModel.SetCallback() {
+//            @Override
+//            public void onSuccess(String msg) {
+//                Log.d("Firebase", "written");
+//            }
+//
+//            @Override
+//            public void onError(Exception e) {
+//                Log.e("Firebase", "fail");
+//            }
+//        });
+//    }
+//
+//    public void getEventTest() {
+//        DataModel model = new DataModel();
+//        Event mockEvent = mockEvent();
+//
+//        model.getEvent("", new DataModel.GetCallback() {
+//            @Override
+//            public void onSuccess(Object obj) {
+//                Log.d("Firebase", "retrieved");
+//
+//                Event event = (Event) obj;
+//                assertEquals(mockEvent.getDate_time(), event.getDate_time());
+//            }
+//
+//            @Override
+//            public void onError(Exception e) {
+//                Log.e("Firebase", "fail");
+//            }
+//        });
+//    }
+}
