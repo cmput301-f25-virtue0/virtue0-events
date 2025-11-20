9f58d0d450d061a34200bdaf0c2e390654cbd300
katietamura <kntamura@ualberta.ca>
2025-11-19 17:57:37 -0700

    added getters so Firestore works

--- LotteryEventApp/app/src/main/java/com/example/lotteryeventapp/EventAdapter.java
+++ LotteryEventApp/app/src/main/java/com/example/lotteryeventapp/EventAdapter.java
@@ -18,7 +18,7 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventViewHol
 
     public interface OnEventClickListener {
         void onEventClick(@NonNull Event event, int position);
-        void onDeleteClick(Event delEvent, int delPosition);
+//        void onDeleteClick(Event delEvent, int delPosition);
     }
 
     private final List<Event> items = new ArrayList<>();
@@ -60,12 +60,23 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventViewHol
         h.ivPoster.setImageResource(R.drawable.lottery); // placeholder
         if (role == 2) { // 2 = Admin
             h.btnDelete.setVisibility(View.VISIBLE);
-            h.btnDelete.setOnClickListener(v -> {
-                clickListener.onDeleteClick(e, h.getBindingAdapterPosition());
-                DataModel dm = new DataModel();
-                dm.deleteEvent(e);
-
-            });
+//            h.btnDelete.setOnClickListener(v -> {
+//                clickListener.onDeleteClick(e, h.getBindingAdapterPosition());
+////                dm.deleteEvent(e);
+//                DataModel model = new DataModel();
+//                model.deleteEvent(e, new DataModel.DeleteCallback() {
+//                    @Override
+//                    public void onSuccess() {
+//
+//                    }
+//
+//                    @Override
+//                    public void onError(Exception e) {
+//
+//                    }
+//                });
+//
+//            });
         } else {
             h.btnDelete.setVisibility(View.INVISIBLE);
         }
