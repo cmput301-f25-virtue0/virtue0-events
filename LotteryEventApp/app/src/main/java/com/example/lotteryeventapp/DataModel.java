1bff7ae2c8f735519838c7bbe741439b7c4383bd
katietamura <kntamura@ualberta.ca>
2025-11-24 14:21:16 -0700

    sends Rejections and Invitations

--- LotteryEventApp/app/src/main/java/com/example/lotteryeventapp/DataModel.java
+++ LotteryEventApp/app/src/main/java/com/example/lotteryeventapp/DataModel.java
@@ -448,8 +448,35 @@ public class DataModel extends TModel<TView>{
         getAllEvents(cb, false);
     }
 
-    public void getUsableEntrants(Event event, GetCallback cb){
+    public void getUsableWaitlistEntrants(Event event, DataModel.GetCallback cb){
         List entrantsIds = event.getWaitlist();
+//        ArrayList<> filterObjects = new ArrayList<>;
+        ArrayList<Entrant> entrants = new ArrayList<>();
+        if (entrantsIds.size() == 0)
+
+//        Filter filter = Filter.equalTo(FieldPath.documentId(), entrants);
+            this.entrants.whereIn(FieldPath.documentId(),   entrantsIds)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    EntrantDataHolder data = new EntrantDataHolder(document.getData(),document.getId());
+                                    entrants.add(data.createEntrantInstance());
+                                }
+                                cb.onSuccess(entrants);
+                            } else {
+                                Log.d("Firestore", "Error getting documents: ", task.getException());
+                                cb.onError(task.getException());
+                            }
+                        }
+                    });
+    }
+
+
+    public void getUsableInvitedListEntrants(Event event, DataModel.GetCallback cb){
+        List entrantsIds = event.getInvited_list();
 //        ArrayList<> filterObjects = new ArrayList<>;
         ArrayList<Entrant> entrants = new ArrayList<>();
 
@@ -472,7 +499,6 @@ public class DataModel extends TModel<TView>{
                     }
                 });
     }
-
     public void getUsableEvents(Organizer organizer, GetCallback cb){
         List eventsIds = organizer.getEvents();
 //        ArrayList<> filterObjects = new ArrayList<>;
