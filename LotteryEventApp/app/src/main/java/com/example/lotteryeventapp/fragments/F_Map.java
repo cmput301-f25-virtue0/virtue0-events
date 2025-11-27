00094c02fc77b56dd71d8e8df54d573d3f1986a4
SpaceLobster210 <SpaceLobster188@gmail.com>
2025-11-27 00:31:06 -0700

    Update F_Map.java
    set up framework for geolocation. Not yet working due to waiting for google cloud verification

--- LotteryEventApp/app/src/main/java/com/example/lotteryeventapp/fragments/F_Map.java
+++ LotteryEventApp/app/src/main/java/com/example/lotteryeventapp/fragments/F_Map.java
@@ -1,22 +1,31 @@
 package com.example.lotteryeventapp.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.lotteryeventapp.Entrant;
 import com.example.lotteryeventapp.MainActivity;
 import com.example.lotteryeventapp.DataModel;
 import com.example.lotteryeventapp.Event;
 import com.example.lotteryeventapp.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.appbar.MaterialToolbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class F_Map extends Fragment implements OnMapReadyCallback {
 
@@ -25,6 +34,8 @@ public class F_Map extends Fragment implements OnMapReadyCallback {
     private Event event;
 
     private GoogleMap gmap;
+    private TextView tvWaitlistSize;
+    private TextView tvCount;
 
     public static F_Map newInstance(int myRole){
         F_Map fragment = new F_Map();
@@ -73,6 +84,38 @@ public class F_Map extends Fragment implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
 
         gmap = googleMap;
+        ArrayList<Entrant.Profile> myEntrants = getEntrants();
 
+        int minx = 99999;
+        int maxx = -99999;
+        int miny = 99999;
+        int maxy = -99999;
+
+        for (int i = 0; i < myEntrants.size(); i++) {
+            Entrant.Profile thisProfile = myEntrants.get(i);
+            //todo: get location from profile
+            int x = 0;
+            int y = 0;
+            String name = "placeholder";
+
+            LatLng pos = new LatLng(x, y);
+            if (x < minx) {minx = x;}
+            if (x > maxx) {maxx = x;}
+            if (y < miny) {miny = y;}
+            if (y > maxy) {maxy = y;}
+
+            gmap.addMarker(new MarkerOptions().position(pos).title(name));
+        }
+        //todo: get zoom & position based on max and min x & y
+        int posx = minx + (maxx - minx) / 2;
+        int posy = miny + (maxy - miny) / 2;
+        LatLng position = new LatLng(posx, posy);
+        int zoom = 1;
+        gmap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, zoom));
+    }
+
+    public ArrayList<Entrant.Profile> getEntrants() {
+        //todo: add a way of fetching all entrants from the waiting list, chosen, and enrolled.
+        return new ArrayList<Entrant.Profile>();
     }
 }
