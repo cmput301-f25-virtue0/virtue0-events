bf6a9a9538693f6ef533d445ca2c740f803d16c6
rCaptainKT <144297314+rCaptainKT@users.noreply.github.com>
2025-11-29 19:19:30 -0700

    added pagination for all entrants and all organizers

--- LotteryEventApp/app/src/main/java/com/example/lotteryeventapp/AllEntrantsPagination.java
+++ LotteryEventApp/app/src/main/java/com/example/lotteryeventapp/AllEntrantsPagination.java
@@ -1,6 +1,5 @@
 package com.example.lotteryeventapp;
 
-import com.google.android.gms.common.data.DataHolder;
 import com.google.firebase.firestore.FieldPath;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
@@ -10,13 +9,13 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
 import java.util.Comparator;
 
-public class AllOrganizersPagination extends FirestorePagination {
-    public AllOrganizersPagination(int pageSize) {
+public class AllEntrantsPagination extends FirestorePagination {
+    public AllEntrantsPagination(int pageSize) {
         super();
         this.pageSize = pageSize;
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        Query query = db.collection("organizers")
+        Query query = db.collection("entrants")
                 .orderBy(FieldPath.documentId(), Query.Direction.DESCENDING);
 
         this.baseQuery = query;
@@ -26,13 +25,13 @@ public class AllOrganizersPagination extends FirestorePagination {
 
     @Override
     protected void createPage(QuerySnapshot snapshot, PaginationCallback cb) {
-        ArrayList<Organizer> organizers = new ArrayList<>();
+        ArrayList<Entrant> entrants = new ArrayList<>();
         for (QueryDocumentSnapshot document : snapshot) {
-            OrganizerDataHolder data = new OrganizerDataHolder(document.getData(), document.getId());
-            organizers.add(data.createOrganizerInstance());
+            EntrantDataHolder data = new EntrantDataHolder(document.getData(), document.getId());
+            entrants.add(data.createEntrantInstance());
         }
-        organizers.sort(Comparator.comparing(Organizer::getUid));
+        entrants.sort(Comparator.comparing(Entrant::getUid));
 
-        cb.onGetPage(true, organizers);
+        cb.onGetPage(true, entrants);
     }
 }
